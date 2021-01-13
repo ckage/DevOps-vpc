01807ef6d8d96d99b6ca8ec19b3fb253bc02e5b9
ckage <76137334+ckage@users.noreply.github.com>
2021-01-13 10:03:35 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const env = process.env
 var express = require('express')
 var os = require("os");
 var hostname = os.hostname();
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send('The value of PORT is:', process.env.game.properties)
+  res.send('')
 })
 app.listen(8080, function() {
-  console.log(process.env);
+  console.log('');
 })
